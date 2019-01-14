3c69de6a90866144fa1e0f1cf04d48c85d652619
buntafujikawa <bunta.fujikawa@gmail.com>
2019-01-14 23:32:20 +0900

    add post

---
+++ database/migrations/2019_01_14_232811_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class CreatePostsTable.
+ */
+class CreatePostsTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('posts', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('category_id');
+            $table->text('content');
+
+            $table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('posts');
+	}
+}
